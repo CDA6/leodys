72a4106ffb2ffcacdb2af7f9d97b409fed6437a9
YoLaub <johngreta904@gmail.com>
2026-01-12 12:04:40 +0100

    feat: entity referent

---
+++ lib/features/notification/domain/entities/referent.dart
@@ -0,0 +1,8 @@
+class Referent {
+  final String id;
+  final String name;
+  final String email;
+  final String role;
+
+  const Referent({required this.id, required this.name, required this.email, required this.role});
+}
