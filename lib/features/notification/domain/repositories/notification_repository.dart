23a5b9fd120d1dee74b2c520d653e323b902e1c7
YoLaub <johngreta904@gmail.com>
2026-01-13 14:00:38 +0100

    Feat: ajout referent et message, logique email

--- lib/features/notification/domain/repositories/notification_repository.dart
+++ lib/features/notification/domain/repositories/notification_repository.dart
@@ -1,11 +1,11 @@
-import '../entities/referent.dart';
+import '../entities/message_entity.dart';
+import '../entities/referent_entity.dart';
 
 abstract class NotificationRepository {
-  Future<void> sendEmailToReferent({
-    required Referent referent,
-    required String subject,
-    required String body,
-  });
-
+  Future<void> sendEmailToReferent({required Referent referent, required String subject, required String body});
   Future<List<Referent>> getReferents();
+  Future<void> addReferent(Referent referent);
+  Future<void> deleteReferent(String id);
+  Future<List<MessageEntity>> getMessageHistory();
+  Future<void> saveMessage(MessageEntity message);
 }
