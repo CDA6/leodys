c12e36629a54cb63b1638d1d7da00eb4f0a2e183
Guillaume Le Gall <glegall@greta-bretagne-sud.fr>
2026-01-14 09:04:06 +0100

    refactor(map): following the review of the last MR : add "usecase" for WatchUserLocation classname and filename.

--- lib/features/map/domain/useCases/watch_user_location_usecase.dart
+++ lib/features/map/domain/useCases/watch_user_location_usecase.dart
@@ -1,11 +1,10 @@
 import 'package:Leodys/features/map/domain/entities/geo_position.dart';
 import 'package:Leodys/features/map/domain/repositories/location_repository.dart';
 
-class WatchUserLocation
-{
+class WatchUserLocationUseCase {
   final ILocationRepository repository;
 
-  WatchUserLocation(this.repository);
+  WatchUserLocationUseCase(this.repository);
 
   Stream<GeoPosition> call() {
     return repository.watchPosition();
--- lib/features/map/presentation/viewModel/map_view_model.dart
+++ lib/features/map/presentation/viewModel/map_view_model.dart
@@ -1,25 +1,22 @@
 import 'dart:async';
 
 import 'package:Leodys/features/map/domain/entities/geo_position.dart';
-import 'package:Leodys/features/map/domain/useCases/watch_user_location.dart';
+import 'package:Leodys/features/map/domain/useCases/watch_user_location_usecase.dart';
 import 'package:Leodys/utils/app_logger.dart';
 
-class MapViewModel
-{
-  final WatchUserLocation watchUserLocation;
+class MapViewModel {
+  final WatchUserLocationUseCase watchUserLocation;
   Stream<GeoPosition> get positionStream => watchUserLocation();
 
   GeoPosition? currentPosition;
 
   MapViewModel(this.watchUserLocation);
 
-  void handleLeaving()
-  {
+  void handleLeaving() {
     AppLogger().info("Leaving map page");
   }
 
-  void handleLanding()
-  {
+  void handleLanding() {
     AppLogger().info("Landing on map page");
   }
 }
