ee1d16869cfc114e438ca571af508b37a46a195d
arnaud fgr. <arnaudfgr.dev@gmail.com>
2026-01-16 22:15:39 +0100

    FIX: router with GetIt

---
+++ lib/common_widgets/feature_item.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class FeatureItem extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
+
+  const FeatureItem({super.key, required this.icon, required this.label, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 50, color: Colors.blue),
+          const SizedBox(height: 8),
+          Text(
+            label,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/authentication/presentation/pages/auth/home_page.dart
+++ lib/features/authentication/presentation/pages/auth/home_page.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:leodys/common_widgets/appbar/global_appbar.dart';
+
+import '../../../../../common_widgets/appbar/global_appbar.dart';
+import '../../../../../common_widgets/feature_item.dart';
+import '../../../../map/presentation/screen/map_screen.dart';
+import '../../../../ocr-reader/presentation/screens/ocr_type_selection.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -25,11 +30,41 @@ class HomePage extends StatelessWidget {
               const SizedBox(height: 16),
               const Text(
                 'Cette application est accessible sans connexion.\n'
-                'Cliquez sur "Se connecter" dans la barre du haut pour vous authentifier.',
+                    'Cliquez sur "Se connecter" dans la barre du haut pour vous authentifier.',
                 style: TextStyle(fontSize: 16, color: Colors.grey),
                 textAlign: TextAlign.center,
               ),
               const SizedBox(height: 48),
+              Expanded(
+                child: GridView.count(
+                  crossAxisCount: 2, // Nombre de colonnes
+                  children: [
+                    FeatureItem(
+                      icon: Icons.map,
+                      label: 'Carte',
+                      onTap: () {
+                        Navigator.pushNamed(context, MapScreen.route);
+                      },
+                    ),
+                    FeatureItem(
+                      icon: Icons.camera_alt,
+                      label: 'OCR Reader',
+                      onTap: () {
+                        Navigator.pushNamed(context, OcrTypeSelectionScreen.route);
+                      },
+                    ),
+                    FeatureItem(
+                      icon: Icons.mic,
+                      label: 'Voice Clock',
+                      onTap: () {
+                        // Navigue vers la route appropriée pour Voice Clock
+                        Navigator.pushNamed(context, '/voice-clock');
+                      },
+                    ),
+                    // Ajoutez d'autres fonctionnalités ici
+                  ],
+                ),
+              ),
             ],
           ),
         ),
