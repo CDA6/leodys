9fe5da3941e00cdd9e6f13ba6584f35dd9bc8361
arnaud fgr. <arnaudfgr.dev@gmail.com>
2026-01-16 22:15:39 +0100

    FIX: router with GetIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,34 @@
-import 'package:leodys/features/map/data/dataSources/geolocator_datasource.dart';
-import 'package:leodys/features/map/data/repositories/location_repository_impl.dart';
-import 'package:leodys/features/map/presentation/viewModel/map_view_model.dart';
-import 'package:leodys/utils/internet_util.dart';
 import 'package:flutter/material.dart';
-import 'package:leodys/constants/auth_constants.dart';
-import 'package:leodys/features/authentication/presentation/pages/auth/home_page.dart';
+import 'package:leodys/features/ocr-reader/presentation/viewmodels/handwritten_text_viewmodel.dart';
 import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'features/authentication/domain/services/auth_service.dart';
+import 'utils/internet_util.dart';
+import 'constants/auth_constants.dart';
+
+import 'features/ocr-reader/injection_container.dart' as ocr_reader;
+import 'features/ocr-reader/presentation/screens/ocr_type_selection.dart';
+import 'features/ocr-reader/presentation/viewmodels/printed_text_viewmodel.dart';
+
 import 'features/map/data/dataSources/geolocator_datasource.dart';
 import 'features/map/data/repositories/location_repository_impl.dart';
+import 'features/map/presentation/viewModel/map_view_model.dart';
 import 'features/map/domain/useCases/watch_user_location_usecase.dart';
 import 'features/map/presentation/screen/map_screen.dart';
-import 'features/map/presentation/viewModel/map_view_model.dart';
+import 'features/authentication/presentation/pages/auth/home_page.dart';
+import 'features/authentication/domain/services/auth_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
-  //Init our internet listener and current status
-  await InternetUtil.init();
 
+  await InternetUtil.init();
 
   await Supabase.initialize(
     url: AuthConstants.projectUrl,
     anonKey: AuthConstants.apiKey,
   );
+
+  await ocr_reader.init();
 
   runApp(MyApp());
 }
@@ -34,28 +37,36 @@ class MyApp extends StatelessWidget {
   
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => AuthService(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AuthService(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ocr_reader.sl<PrintedTextViewModel>(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => ocr_reader.sl<HandwrittenTextViewModel>(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Leodys',
         debugShowCheckedModeBanner: false,
         initialRoute: HomePage.route,
-      routes: {
-        HomePage.route: (context) => const HomePage(),
-        MapScreen.route: (context) {
-          final dataSource = GeolocatorDatasource();
-          final repository = LocationRepositoryImpl(dataSource);
-          final useCase = WatchUserLocationUseCase(repository);
-          final viewModel = MapViewModel(useCase);
+        routes: {
+          HomePage.route: (context) => const HomePage(),
+          MapScreen.route: (context) {
+            final dataSource = GeolocatorDatasource();
+            final repository = LocationRepositoryImpl(dataSource);
+            final useCase = WatchUserLocationUseCase(repository);
+            final viewModel = MapViewModel(useCase);
 
-          return MapScreen(viewModel: viewModel);
+            return MapScreen(viewModel: viewModel);
+          },
+          OcrTypeSelectionScreen.route: (context) => const OcrTypeSelectionScreen(),
         },
-      },
-      //initialRoute: LoginPage.route,
-        
       ),
     );
   }
