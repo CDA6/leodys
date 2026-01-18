16caaad73b8809c432ce71d8946ca60e70b411c9
YoLaub <johngreta904@gmail.com>
2026-01-18 03:36:06 +0100

    fix: ajout de plateform_util

--- lib/features/notification/presentation/controllers/voice_controller.dart
+++ lib/features/notification/presentation/controllers/voice_controller.dart
@@ -1,10 +1,20 @@
 import 'package:speech_to_text/speech_to_text.dart';
 
+import '../../../../common/utils/platform_util.dart';
+
 class VoiceController {
   final SpeechToText _speech = SpeechToText();
   bool _isListening = false;
 
   Future<bool> initSpeech() async {
+    if (PlatformUtil.isWindows) {
+      try {
+        return await _speech.initialize();
+      } catch (e) {
+        print("Erreur d'initialisation Voice sur Windows: $e");
+        return false;
+      }
+    }
     return await _speech.initialize();
   }
 
