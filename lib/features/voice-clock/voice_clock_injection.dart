3d760247db3b8a75f07654b0c051d8ba39dd64c6
YoLaub <johngreta904@gmail.com>
2026-01-18 16:40:37 +0100

    feat: view et logique

---
+++ lib/features/voice-clock/voice_clock_injection.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+import 'data/repositories/clock_repository_impl.dart';
+import 'domain/repositories/clock_repository.dart';
+import 'presentation/viewmodel/voice_clock_viewmodel.dart';
+import '../notification/presentation/controllers/tts_controller.dart';
+
+final sl = GetIt.instance;
+
+void init() {
+  sl.registerLazySingleton<IClockRepository>(() => ClockRepositoryImpl());
+  sl.registerFactory(() => VoiceClockViewModel(sl(), TtsController()));
+}
