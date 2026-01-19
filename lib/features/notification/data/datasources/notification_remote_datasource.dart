7b0d4e140764657ab11ff29d78d6ad0ae18dba3a
YoLaub <johngreta904@gmail.com>
2026-01-19 01:11:56 +0100

    refactor: edge function

--- lib/features/notification/data/datasources/notification_remote_datasource.dart
+++ lib/features/notification/data/datasources/notification_remote_datasource.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../../constants/mail_constants.dart';
 import '../../domain/entities/referent_entity.dart';
 
@@ -11,21 +12,28 @@ class NotificationRemoteDataSourceImpl implements NotificationRemoteDataSource {
   static final String _apiKey = MailConstants.apiKeyBrevo;
   static final String _apiUrl = MailConstants.apiUrlBrevo;
   static final String _sender = MailConstants.sender;
+  final _supabase = Supabase.instance.client;
 
 
   @override
   Future<void> sendEmail({required ReferentEntity referent, required String subject, required String body}) async {
-    final response = await http.post(
-      Uri.parse(_apiUrl),
-      headers: {'api-key': _apiKey, 'Content-Type': 'application/json'},
-      body: jsonEncode({
-        'sender': {'name': 'Leodys App', 'email': _sender},
-        'to': [{'email': referent.email, 'name': referent.name}],
-        'subject': subject,
-        'textContent': body,
-      }),
-    );
+    try {
+      // Appel de l'Edge Function au lieu d'un http.post direct
+      final response = await _supabase.functions.invoke(
+        'super-email-brevo',
+        body: {
+          'referentEmail': referent.email,
+          'referentName': referent.name,
+          'subject': subject,
+          'body': body,
+        },
+      );
 
-    if (response.statusCode != 201) throw Exception('Erreur Brevo: ${response.body}');
+      if (response.status != 200 && response.status != 201) {
+        throw Exception('Erreur Edge Function: ${response.data}');
+      }
+    } catch (e) {
+      throw Exception('Erreur de communication avec le serveur: $e');
+    }
   }
 }
