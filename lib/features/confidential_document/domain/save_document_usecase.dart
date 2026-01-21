831bb5fd392b8f470b55898cf31646025acd5c15
martha <mdurand@greta-bretagne-sud.fr>
2026-01-21 20:32:05 +0100

    FEAT - methode synchronisation
    # Conflicts:
    #	pubspec.lock
    #	pubspec.yaml

--- lib/features/confidential_document/domain/save_document_usecase.dart
+++ lib/features/confidential_document/domain/save_document_usecase.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:cryptography/cryptography.dart';
 import 'package:flutter/foundation.dart';
 import 'package:leodys/features/confidential_document/data/local_storage_repository.dart';
@@ -10,14 +7,16 @@ import 'package:leodys/features/confidential_document/domain/encryption_service.
 import 'package:leodys/features/confidential_document/domain/entity/decryption_result.dart';
 import 'package:leodys/features/confidential_document/domain/entity/picture_download.dart';
 import 'package:leodys/features/confidential_document/domain/entity/save_result.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SaveDocumentUsecase {
-  final RemoteStorageRepository _remoteStorageRepository = RemoteStorageRepository();
-  final LocalStorageRepository _localStorageRepository = LocalStorageRepository();
+  final RemoteStorageRepository _remoteStorageRepository =
+      RemoteStorageRepository();
+  final LocalStorageRepository _localStorageRepository =
+      LocalStorageRepository();
   final EncryptionService _encryptionService = EncryptionService();
   EncryptionSession session = EncryptionSession();
+
   //Get the actual user
   User? get user => Supabase.instance.client.auth.currentUser;
   late final userID = user?.id;
@@ -26,10 +25,14 @@ class SaveDocumentUsecase {
     return user?.email;
   }
 
-  Future<SaveResult> saveImage(Uint8List bytes, String title, SecretKey key) async {
+  Future<SaveResult> saveImage(
+    Uint8List bytes,
+    String title,
+    SecretKey key,
+  ) async {
     bool localSuccess = false;
     bool remoteSuccess = false;
-    try{
+    try {
       if (bytes.isEmpty) throw Exception("L'image est vide");
       if (title.trim().isEmpty) throw Exception("Le titre est invalide");
       final encryptByte = await _encryptionService.encryptData(bytes, key);
@@ -42,37 +45,45 @@ class SaveDocumentUsecase {
           print("Erreur disque : $e");
         }
       } else {
-        localSuccess = true; // On considère OK sur Web car pas de stockage local
+        localSuccess =
+            true; // On considère OK sur Web car pas de stockage local
       }
       try {
         await _remoteStorageRepository.uploadDocument(encryptByte, title);
         remoteSuccess = true;
       } catch (e) {
         print("Erreur réseau : $e");
-      };
+      }
+      ;
       if (localSuccess && remoteSuccess) return SaveResult.fullSuccess;
       if (localSuccess) return SaveResult.localOnly;
       return SaveResult.failure;
-    }catch(e) {
-     return SaveResult.failure;
+    } catch (e) {
+      return SaveResult.failure;
     }
   }
 
-  Future<DecryptionResult?> getAllImages() async {
+  Future<DecryptionResult?> getAllImages(bool hasConnection) async {
     print("Lancement recherhce image dans usecase");
     final SecretKey? key = session.key;
     int errorCount = 0;
-List<PictureDownload> listPicture = [];
-    final List<String> listTitles = await _remoteStorageRepository.getListTitle();
-    if(listTitles.isEmpty){
-      return null;
-    }
-    for (String title in listTitles) {
-      print("boucle en cours");
-      Uint8List? encryptedData = await _remoteStorageRepository.getImage(title);
-      if(encryptedData != null && encryptedData.isNotEmpty){
-        Uint8List? byte = await _encryptionService.decryptData(encryptedData, key!);
-        if(byte != null) {
+    List<PictureDownload> listPicture = [];
+
+    //Récupération du local hors-ligne
+    if (!hasConnection) {
+      final Map<String, Uint8List> listFile = await _localStorageRepository
+          .getAllEncryptedFiles();
+      if (listFile.isEmpty) {
+        return null;
+      }
+      for (var file in listFile.entries) {
+        final String title = file.key;
+        final Uint8List encryptedData = file.value;
+        Uint8List? byte = await _encryptionService.decryptData(
+          encryptedData,
+          key!,
+        );
+        if (byte != null) {
           PictureDownload image = PictureDownload(byte, title);
           listPicture.add(image);
         } else {
@@ -80,6 +91,34 @@ List<PictureDownload> listPicture = [];
         }
       }
     }
+    ///Récupération seulement sur le remote storage et pas de local
+    if (hasConnection && kIsWeb) {
+      final List<String> listTitles = await _remoteStorageRepository
+          .getListTitle();
+      if (listTitles.isEmpty) {
+        return null;
+      }
+      for (String title in listTitles) {
+        print("boucle en cours");
+        Uint8List? encryptedData = await _remoteStorageRepository.getImage(
+          title,
+        );
+        if (encryptedData != null && encryptedData.isNotEmpty) {
+          Uint8List? byte = await _encryptionService.decryptData(
+            encryptedData,
+            key!,
+          );
+          if (byte != null) {
+            PictureDownload image = PictureDownload(byte, title);
+            listPicture.add(image);
+          } else {
+            errorCount++;
+          }
+        }
+      }
+    }
+    //TODO local et en ligne
+
     return DecryptionResult(listPicture, errorCount);
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,31 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import connectivity_plus
 import file_selector_macos
 import flutter_secure_storage_darwin
-import path_provider_foundation
+import flutter_tts
+import geolocator_apple
+import local_auth_darwin
+import mobile_scanner
+import package_info_plus
+import share_plus
 import shared_preferences_foundation
+import speech_to_text
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
