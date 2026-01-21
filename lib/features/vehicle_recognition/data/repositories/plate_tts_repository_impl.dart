e16a3643b82132313460f0818fad0bf4082c0bd4
Tat <ttat@greta-bretagne-sud.fr>
2026-01-21 17:46:46 +0100

    interface fini

---
+++ lib/features/vehicle_recognition/data/repositories/plate_tts_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:leodys/features/vehicle_recognition/data/services/plate_tts_service.dart';
+import 'package:leodys/features/vehicle_recognition/domain/models/reader_config.dart';
+import 'package:leodys/features/vehicle_recognition/domain/repositories/plate_tts_repository.dart';
+
+
+class PlateTtsRepositoryImpl implements PlateTtsRepository{
+
+  final PlateTtsService plateTtsService;
+  PlateTtsRepositoryImpl(this.plateTtsService);
+
+  @override
+  Future<void> play(String text, ReaderConfig config) async {
+
+    await plateTtsService.speak
+      (
+        text: text,
+        languageCode: config.languageCode,
+        speechRate: config.speechRate,
+        pitch: config.pitch
+    );
+
+  }
+
+  @override
+  Future<void> stop() async{
+    await plateTtsService.stop();
+  }
+
+
+}
