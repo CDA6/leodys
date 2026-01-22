5a74c8393410f7f98e3887125c0d062a03de5836
Tat <ttat@greta-bretagne-sud.fr>
2026-01-22 23:37:23 +0100

    fin api debut usecases

---
+++ lib/features/vehicle_recognition/domain/models/vehicle_info.dart
@@ -0,0 +1,12 @@
+class VehicleInfo {
+
+  final String plate;
+  final String make;
+  final String model;
+
+  const VehicleInfo({
+    required this.plate,
+    required this.make,
+    required this.model,
+  });
+}
