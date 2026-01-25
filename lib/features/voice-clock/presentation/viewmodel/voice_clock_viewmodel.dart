cfc09ba03c5596e579765378cdbff9e767eb4fa9
YoLaub <johngreta904@gmail.com>
2026-01-25 20:47:05 +0100

    fix: tts widget

--- lib/features/voice-clock/presentation/viewmodel/voice_clock_viewmodel.dart
+++ lib/features/voice-clock/presentation/viewmodel/voice_clock_viewmodel.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import '../../../vocal_notes/data/services/speech_service.dart';
 import '../../domain/repositories/clock_repository.dart';
 import '../../domain/entities/clock_config.dart';
-import '../../../notification/presentation/controllers/tts_controller.dart';
+import '../../../vocal_notes/data/services/speech_service.dart';
+import '../../../vocal_notes/injection_container.dart';
 
 class VoiceClockViewModel extends ChangeNotifier {
   final IClockRepository _repository;
-  final TtsController _ttsController;
 
   ClockType _selectedType = ClockType.digital;
   DateTime _currentTime = DateTime.now();
 
-  VoiceClockViewModel(this._repository, this._ttsController) {
+
+  VoiceClockViewModel(this._repository) {
     _repository.getCurrentTime().listen((time) {
       _currentTime = time;
       notifyListeners();
@@ -26,9 +28,4 @@ class VoiceClockViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> speakTime() async {
-    final format = DateFormat('HH:mm');
-    final timeStr = "Il est exactement ${format.format(_currentTime)}";
-    await _ttsController.speak(timeStr);
-  }
 }
--- lib/features/voice-clock/voice_clock_injection.dart
+++ lib/features/voice-clock/voice_clock_injection.dart
@@ -2,11 +2,10 @@ import 'package:get_it/get_it.dart';
 import 'data/repositories/clock_repository_impl.dart';
 import 'domain/repositories/clock_repository.dart';
 import 'presentation/viewmodel/voice_clock_viewmodel.dart';
-import '../notification/presentation/controllers/tts_controller.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
   sl.registerLazySingleton<IClockRepository>(() => ClockRepositoryImpl());
-  sl.registerFactory(() => VoiceClockViewModel(sl(), TtsController()));
+  sl.registerFactory(() => VoiceClockViewModel(sl()));
 }
