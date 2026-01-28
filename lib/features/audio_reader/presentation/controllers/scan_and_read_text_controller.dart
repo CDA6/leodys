c3afe08c0c76a6fbf3c59db0d21fcdad709dce5a
YoLaub <johngreta904@gmail.com>
2026-01-28 10:02:47 +0100

    correction

--- lib/features/audio_reader/presentation/controllers/scan_and_read_text_controller.dart
+++ lib/features/audio_reader/presentation/controllers/scan_and_read_text_controller.dart
@@ -4,7 +4,7 @@ import '../../domain/usecases/scan_and_read_text_usecase.dart';
 
 ///Classe controller de la fonctionnalité scan et lecture audio.
 ///Il étend la classe changeNotifier
-///afin de notifier l'interface utilisateur qu'il y a un changement d'état,
+///afin de notifier l'interfaces utilisateur qu'il y a un changement d'état,
 ///grace à la classe notifyListeners()
 class ScanAndReadTextController extends ChangeNotifier{
 
@@ -20,9 +20,9 @@ class ScanAndReadTextController extends ChangeNotifier{
   ///Lancer le scan du fichier et la lecture
   Future<void> execute (String imagePath, ReaderConfig config) async {
 
-    isLoading = true; // prevenir d'une action longue est en cours. Evite que l'interface se fige
+    isLoading = true; // prevenir d'une action longue est en cours. Evite que l'interfaces se fige
     message='';
-    notifyListeners(); // informe l'interface d'un changement d'état
+    notifyListeners(); // informe l'interfaces d'un changement d'état
 
     try {
       await scanAndReadTextUsecase.execute(imagePath, config);
