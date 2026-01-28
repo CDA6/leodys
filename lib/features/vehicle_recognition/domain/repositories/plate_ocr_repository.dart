ccc47e1abbca8b4e66fced06e50301c78cd5df64
Tat <ttat@greta-bretagne-sud.fr>
2026-01-28 08:32:51 +0100

    refacto

--- lib/features/vehicle_recognition/domain/repositories/plate_ocr_repository.dart
+++ lib/features/vehicle_recognition/domain/repositories/plate_ocr_repository.dart
@@ -5,6 +5,4 @@ abstract class PlateOcrRepository {
   ///  Méthode asynchrone qui retourne une chaine de caracteres reconnu dans l'image
   ///  imagePath est le chemin de l'image à analyser
   Future<String?> extractTextFromImage(String imagePath);
-
-
 }
