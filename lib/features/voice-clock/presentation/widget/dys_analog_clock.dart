776994a5a6fb17a546d83616ffb3ab29bb60057f
YoLaub <johngreta904@gmail.com>
2026-01-29 22:33:34 +0100

    feat: new clock

---
+++ lib/features/voice-clock/presentation/widget/dys_analog_clock.dart
@@ -0,0 +1,91 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+class DysAnalogClock extends StatelessWidget {
+  final DateTime time;
+  final double size;
+
+  const DysAnalogClock({super.key, required this.time, this.size = 250});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: Size(size, size),
+      painter: _ClockPainter(time),
+    );
+  }
+}
+
+class _ClockPainter extends CustomPainter {
+  final DateTime time;
+
+  _ClockPainter(this.time);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+    final radius = size.width / 2;
+
+    // 1. Dessin du cadran
+    final paintCircle = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+    canvas.drawCircle(center, radius, paintCircle);
+
+    final paintOutline = Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 4;
+    canvas.drawCircle(center, radius, paintOutline);
+
+    // 2. Dessin des chiffres (Haute lisibilité)
+    const textStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+    );
+
+    for (int i = 1; i <= 12; i++) {
+      final angle = (i * 30 - 90) * pi / 180;
+      final textOffset = Offset(
+        center.dx + (radius - 30) * cos(angle),
+        center.dy + (radius - 30) * sin(angle),
+      );
+
+      final textPainter = TextPainter(
+        text: TextSpan(text: '$i', style: textStyle),
+        textDirection: TextDirection.ltr,
+      )..layout();
+
+      textPainter.paint(canvas, textOffset - Offset(textPainter.width / 2, textPainter.height / 2));
+    }
+
+    // 3. Aiguille des HEURES (Courte et large - Bleue)
+    final hourAngle = (time.hour % 12 + time.minute / 60) * 30 * pi / 180 - pi / 2;
+    _drawHand(canvas, center, hourAngle, radius * 0.5, 8, Colors.blue.shade900);
+
+    // 4. Aiguille des MINUTES (Longue et fine - Rouge)
+    final minuteAngle = (time.minute + time.second / 60) * 6 * pi / 180 - pi / 2;
+    _drawHand(canvas, center, minuteAngle, radius * 0.8, 5, Colors.red.shade900);
+
+    // Point central
+    canvas.drawCircle(center, 6, Paint()..color = Colors.black);
+  }
+
+  void _drawHand(Canvas canvas, Offset center, double angle, double length, double width, Color color) {
+    final paint = Paint()
+      ..color = color
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = width;
+
+    final endPoint = Offset(
+      center.dx + length * cos(angle),
+      center.dy + length * sin(angle),
+    );
+
+    canvas.drawLine(center, endPoint, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant _ClockPainter oldDelegate) => oldDelegate.time != time;
+}
